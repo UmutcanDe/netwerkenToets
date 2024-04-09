2477ee52a4b85e70e97373e7e1370c29422815f3
UmutcanDe <Umutcan.demirez@student.graafschapcollege.nl>
2024-04-09 09:10:42 +0200

    test

--- config/db_config.php
+++ config/db_config.php
@@ -1,11 +1,11 @@
 <?php
 
 $dbhost = 'localhost:3306';
-$dbuser = '';
+$dbuser = 'root';
 $dbpass = '';
 $dbname = 'net24udemirez_netwerken_2024';
 
-$conn = new mysqli($dbhost,$dbuser);
+$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if($conn->connect_error)
 {
 	die('Failed Connect ! '.$conn->connect_error);
